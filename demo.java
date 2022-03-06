63e4554bea6296f0f7577ccfcdd76260efb1f83c
JIYANIJAY3 <jayjiyani.inexture@gmail.com>
2022-03-06 15:42:38 +0530

    modify

--- demo.java
+++ demo.java
@@ -1,10 +1,6 @@
 class demo
 {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World project4");
-=======
         System.out.println("Hello World devlopment");
->>>>>>> devlopment
     }
 }
